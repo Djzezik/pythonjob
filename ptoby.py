f65e5044d8136f3803c4c9990fcc5d8ba929fa0e
Zezik <Djzezik@mail.ru>
2022-03-12 21:49:34 +0300

    начал выполнять задания, рисова

---
+++ ptoby.py
@@ -0,0 +1,9 @@
+# корректный ввод пользователя
+while True:
+    user_input = input('Введите 42 >> ')
+    result = int(user_input)
+    if result == 42:
+        print('Спасибо за сотрудничество!')
+        break
+    else:
+        print('Я просил 42, а Вы ввели', result, 'Попробуйте еще раз...')
