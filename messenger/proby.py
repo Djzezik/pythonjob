9d9df6d9aa081b5ef31274be70fd8b2279cd69b2
Zezik <Djzezik@mail.ru>
2022-03-17 22:49:16 +0300

    начали делать чат

---
+++ messenger/proby.py
@@ -0,0 +1,26 @@
+def print_message(soobsenie):  # обьявление функции сообщения на печать
+    print(soobsenie ["sender"])
+    print(soobsenie ["time"])
+    print(soobsenie ["text"])
+
+
+# словарь
+message1 = {
+    'text': 'как дела',
+    'sende': 'Михаил',
+    'time': '19:20'
+
+}
+
+massage2 = {
+    'text': 'отлично',
+    'sende': 'Василий',
+    'time': '19:21'
+}
+
+
+
+
+
+print_message()
+print_message()
