01d4f86563e4a0ae0e25ba49d5ecbcf385122510
Djzez <Zezik@mail.ru>
2022-10-26 11:14:11 +0300

    Сделал задание с радиосом

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -22,19 +22,19 @@ moscow_london = (moscow[0]- london[0])** 2 + (moscow[1]- london[1]) ** 2
 moscow_paris = (moscow[0]- paris[0])** 2 + (moscow[1]- paris[1]) ** 2
 london_paris = (london[0]- paris[0])** 2 + (london[1]- paris[1]) ** 2
 
-distances ['Moscow'] = {}
-distances ['Moscow']['london'] = moscow_london
-distances ['Moscow']['Paris'] = moscow_paris
+distances['Moscow'] = {}
+distances['Moscow']['london'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
 
-distances ['london'] = {}
-distances ['london']['Moscow'] = moscow_london
-distances ['london']['Paris'] = london_paris
+distances['london'] = {}
+distances['london']['Moscow'] = moscow_london
+distances['london']['Paris'] = london_paris
 
-distances ['Paris'] = {}
-distances ['Paris']['Moscow'] = moscow_paris
-distances ['Paris']['London'] = london_paris
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = moscow_paris
+distances['Paris']['London'] = london_paris
 
-pprint (distances)
+pprint(distances)
 
 
 
