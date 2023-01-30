23e8ba8529e0625dbb9f04af37f11fdd9e1d89d4
linuxjob <djzezik@mail.ru>
2023-01-30 14:03:37 +0300

    сделел первое задание с рисованием

--- ptoby.py
+++ ptoby.py
@@ -1,2 +1,12 @@
-print('hello')
-#проверка git linux
+import simple_draw as sd
+circle (center_position, radius=50, color=COLOR_YELLOW, width=1):
+def bubble (point, step, color_):
+    radius = 50
+    for _ in range(3):
+        radius +=step
+        sd.circle(center_position=point, radius=radius, width=2, color=color_)
+
+
+for _ in range(100):
+    point = sd.random_point()
+    bubble(point=point, step=5)
