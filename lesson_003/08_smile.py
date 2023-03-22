b6b1df1bbe9ed604da9c4e2df1efbea8c107abec
linuxjob <djzezik@mail.ru>
2023-03-22 11:34:05 +0300

    сделал задание с трегульникми

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
@@ -9,5 +9,11 @@ import simple_draw
 # Вывести 10 смайликов в произвольных точках экрана.
 
 # TODO здесь ваш код
+
+def bubble (point, step):
+    radius = 50
+    for _ in range(3):
+        radius +=step
+        sd.circle(center_position=point, radius=radius, width=2)
 
 simple_draw.pause()
