cd2fc7e946634e40682fe392c8116a0f3b44c02f
linuxjob <djzezik@mail.ru>
2023-03-22 14:05:33 +0300

    сделал задания с фигурами

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -28,6 +28,84 @@ import simple_draw as sd
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
 # TODO здесь ваш код
+# def triangle(point, angle, length): # функция рисования трехугольника
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=3)
+#     v3.draw()
+#
+#
+# point_0 = sd.get_point(400, 400)
+# triangle(point=point_0, angle=0, length=100)
+#
+#
+#
+#
+# def square (point, angle, length): # функция рисования квадрата
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=3)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 270, length=length, width=3)
+#     v4.draw()
+#
+#
+# point_0 = sd.get_point(100, 100)
+# square(point=point_0, angle=0, length=100)
+#
+#
+# def pentagon(point, angle, length): # функция рисования пятиугольника
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length=length, width=3)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=3)
+#     v4.draw()
+#
+#     v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 288, length=length, width=3)
+#     v5.draw()
+#
+# point_0 = sd.get_point(100, 300)
+# pentagon(point=point_0, angle=0, length=50)
+#
+# def hexagon(point, angle, length): # функция рисования шестиугольника
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
+#     v4.draw()
+#
+#     v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
+#     v5.draw()
+#
+#     v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length=length, width=3)
+#     v6.draw()
+#
+# point_0 = sd.get_point(300, 100)
+# hexagon(point=point_0, angle=0, length=50)
+#
+#
+# sd.pause()
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
@@ -51,6 +129,10 @@ import simple_draw as sd
 # А теперь - сколько надо работы что бы внести изменения в код? Выгода на лицо :)
 # Поэтому среди программистов есть принцип D.R.Y. https://clck.ru/GEsA9
 # Будьте ленивыми, не используйте копи-пасту!
+
+def vector (vector_start, legth, angle):
+    v = sd.get_vector(vector_start, legth, angle)
+    return v.end_point
 
 
 sd.pause()
