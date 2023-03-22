1f98d8156c267c83b191e6ac82ef76dd9eb91517
linuxjob <djzezik@mail.ru>
2023-03-22 14:36:37 +0300

    делаю задания с фигурами

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import simple_draw as sd
+from pprint import pprint
 
 # Добавить цвет в функции рисования геом. фигур. из упр lesson_004/01_shapes.py
 # (код функций скопировать сюда и изменить)
@@ -15,5 +16,92 @@ import simple_draw as sd
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
 # TODO здесь ваш код
+color = {1:'COLOR_RED',
+         2:"COLOR_ORANGE",
+         3:'COLOR_YELLOW',
+         4:'COLOR_GREEN',
+         5:'COLOR_CYAN',
+         6:'COLOR_BLUE',
+         7:'COLOR_PURPLE'}
 
-sd.pause()
+# pprint( color)
+# colorinput =  input("Введите номер желаемого цвета")
+# colorinput = int(colorinput)
+
+def triangle(point, angle, length, color): # функция рисования трехугольника
+    v1 = sd.line(start_point=point, angle=angle, length=length, width=3, color=sd.COLOR_RED)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=length, width=3)
+    v3.draw()
+
+
+point_0 = sd.get_point(400, 400)
+triangle(point=point_0, angle=0, length=100, color=sd.COLOR_RED)
+
+
+
+
+
+# def square (point, angle, length): # функция рисования квадрата
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=3)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 270, length=length, width=3)
+#     v4.draw()
+#
+#
+# point_0 = sd.get_point(100, 100)
+# square(point=point_0, angle=0, length=100)
+#
+#
+# def pentagon(point, angle, length): # функция рисования пятиугольника
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length=length, width=3)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=3)
+#     v4.draw()
+#
+#     v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 288, length=length, width=3)
+#     v5.draw()
+#
+# point_0 = sd.get_point(100, 300)
+# pentagon(point=point_0, angle=0, length=50)
+#
+# def hexagon(point, angle, length): # функция рисования шестиугольника
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#
+#     v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
+#     v2.draw()
+#
+#     v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
+#     v3.draw()
+#
+#     v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
+#     v4.draw()
+#
+#     v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
+#     v5.draw()
+#
+#     v6 = sd.get_vector(start_point=v5.end_point, angle=angle + 300, length=length, width=3)
+#     v6.draw()
+#
+# point_0 = sd.get_point(300, 100)
+# hexagon(point=point_0, angle=0, length=50)
+# sd.pause()
